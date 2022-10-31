3edd00196fa42078e20ea39a5bc77a320c9cf9d3
Darcy Keys <dkeys8722@gmail.com>
2022-10-31 15:09:20 +1100

    added title text and started to implement loading bar but it is fucking stupid i will do it later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 pygame.init()
+import time
 
 #variable setup#
 run = True
@@ -7,11 +8,14 @@ calculationMode = False
 consumptionMode = False
 mouseX,mouseY = (0,0) #gets cursor position#
 calcStart = False
-button_text_color = (0, 0, 0)
-title_text_color = (0, 0, 0)
+text_color = (0, 0, 0)
 button_color = (40, 81, 148)
 button_over_color = (23, 46, 84)
 button_font = pygame.font.SysFont("Arial",20)
+title_font = pygame.font.SysFont("Arial",50)
+a = 0
+loading_array = [".",".",".",".",".",".",".",".",".","."]
+
 
 #setting up window#
 win =pygame.display.set_mode((600,600))
@@ -22,8 +26,11 @@ pygame.display.flip()
 
 ##defining windows##
 def startWindow():
-    pass
-#blitting the start window
+    global a
+    global loading_array
+    title_text = title_font.render("Fuel Usage Calculator",True, text_color)
+    win.blit(title_text,(100,200))
+    pygame.display.update()
 
 def menuWindow():
     pass
